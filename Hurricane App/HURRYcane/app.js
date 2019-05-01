cf18adffca8afc2276266460202fd2e37777c39e
Shengzhi Wang <shengzhi@Shengzhis-MacBook-Pro.local>
2019-05-01 05:57:23 -0700

    commit message

--- Hurricane App/HURRYcane/app.js
+++ Hurricane App/HURRYcane/app.js
@@ -4,14 +4,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var exphbs  = require('express-handlebars');
+
+
+
+var login = require('./routes/login');
+var index = require('./routes/index');
+var maps = require('./routes/maps');
+var preparations = require('./routes/preparations');
+var trending = require('./routes/trending');
+
 
 var app = express();
 
-// view engine setup
+
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', login.view);
+app.get('/index', index.view);
+app.get('/maps', maps.view);
+app.get('/preparations', preparations.view);
+app.get('/trending', trending.view);
+
+app.listen(3000);
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +41,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', index);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,14 +50,13 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
