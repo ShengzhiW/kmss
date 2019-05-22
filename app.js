444fbd47629f516830ec11b9f349459d977a6697
Kenny Luc <k1luc@ucsd.edu>
2019-05-21 18:41:08 -0700

    added cookies to save location for map

--- app.js
+++ app.js
@@ -3,47 +3,62 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var firebase = require("firebase-admin");
 var fs = require('fs');
 var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:         'RUCao1GFyFBYTgMrIKT54lgvf',
-  consumer_secret:      '8ffCY1gaG8RehObBTWMrJxo5C3fCSg7kwhLTHcQCOM7oyVbt1B',
-  access_token:         '1127724845750374400-Akjmfoy0bZgwTsF1Kwg0vF92czNpwL',
-  access_token_secret:  'dDO2MN3wmbsmSFzWZqemKh5E3Fc2E1xhtmJlhbDuWYWoI',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+    consumer_key: 'RUCao1GFyFBYTgMrIKT54lgvf',
+    consumer_secret: '8ffCY1gaG8RehObBTWMrJxo5C3fCSg7kwhLTHcQCOM7oyVbt1B',
+    access_token: '1127724845750374400-Akjmfoy0bZgwTsF1Kwg0vF92czNpwL',
+    access_token_secret: 'dDO2MN3wmbsmSFzWZqemKh5E3Fc2E1xhtmJlhbDuWYWoI',
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL: true, // optional - requires SSL certificates to be valid.
 });
 
-function grabHurricaneSeasonTweets() {
-  var params = {
-    q: '[ hurricane season, hurricane warning ] since:2011-07-11 -filter:retweets',
-    count: 5,
-    result_type: 'recent',
-    lang: 'en'
-  }
+var tweetsArr = [];
 
-  T.get('search/tweets', params, gotData)
+function grabHurricaneSeasonTweets() {
+    var params = {
+        q: '[ hurricane season, hurricane warning, hurricane] since:2011-07-11 -filter:retweets',
+        count: 5,
+        result_type: 'recent',
+        lang: 'en'
+    }
+
+    T.get('search/tweets', params, gotData)
 
 }
 
 function gotData(err, data, respon) {
-  var tweets = data.statuses;
-  console.log("new");
-  tweets.map((items) => {
-    console.log(items.text);
-  })
-}
+    let tweets = data.statuses;
+    console.log("new");
 
-setInterval(grabHurricaneSeasonTweets, 3000);
+    tweetsArr = [];
+    if (tweets) {
+        // console.log(tweets);
+        tweets.map((items) => {
+            console.log(items.text);
+            tweetsArr.push(items.text);
+        })
+    } else {
+        console.log("no tweets");
+    }
+
+    console.log(tweetsArr);
+
+}
+// setInterval(grabHurricaneSeasonTweets, 3000);
+
+
+
 
 var serviceAccount = require("./serviceAccountKey.json");
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://hurrycane-52d0c.firebaseio.com"
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://hurrycane-52d0c.firebaseio.com"
 });
 
 
@@ -85,7 +100,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({
- 	partialsDir: path.join(__dirname, '/views/partials')
+    partialsDir: path.join(__dirname, '/views/partials')
 }));
 app.set('view engine', 'handlebars');
 
@@ -96,19 +111,23 @@ app.get('/preparations', preparations.view);
 app.get('/trending', trending.view);
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 
-app.get('/facts', (req, res) =>{
-	ref.once("value", function(snapshot) {
-	var data = snapshot.val()[getRandomInt(19)]
-  	res.send(data)
+app.get('/facts', (req, res) => {
+    ref.once("value", function(snapshot) {
+        let i = getRandomInt(19);
+        var data = snapshot.val()[i]
+        console.log(i);
+        res.send(data)
+    });
 });
 
-
+app.get('/tweets', (req, res) => {
+    grabHurricaneSeasonTweets();
+    res.send(tweetsArr);
 });
-
 
 app.listen(3000);
 
@@ -118,7 +137,9 @@ app.listen(3000);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
