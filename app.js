a09e1b4624466f865cb40ce2e2a7eb93ec3da377
Kenny Luc <k1luc@ucsd.edu>
2019-05-29 10:33:09 -0700

    hidden twitter api

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var logger = require('morgan');
 var exphbs = require('express-handlebars');
 var firebase = require("firebase-admin");
 var fs = require('fs');
+
+var config = require('./config');
+
 var Twit = require('twit')
 
 var T = new Twit({
-    consumer_key: 'RUCao1GFyFBYTgMrIKT54lgvf',
-    consumer_secret: '8ffCY1gaG8RehObBTWMrJxo5C3fCSg7kwhLTHcQCOM7oyVbt1B',
-    access_token: '1127724845750374400-Akjmfoy0bZgwTsF1Kwg0vF92czNpwL',
-    access_token_secret: 'dDO2MN3wmbsmSFzWZqemKh5E3Fc2E1xhtmJlhbDuWYWoI',
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret,
     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
     strictSSL: true, // optional - requires SSL certificates to be valid.
 });
