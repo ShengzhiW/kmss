ac3f6d556ac5819bfa452aa0ba920d5d5fb8fc55
kennyyluc <k1luc@ucsd.edu>
2019-06-03 00:13:16 -0700

    Update maps.js

--- routes/maps.js
+++ routes/maps.js
@@ -1,5 +1,7 @@
-// This files renders the map.handlebars page when the 
-// map page is requested by app.js
+/*
+ * This files renders the map.handlebars page when the 
+ * map page is requested by app.js
+ */
 
 exports.view = function(req, res){
   res.render('maps');
