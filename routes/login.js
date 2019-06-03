847a18a401bb82dcb70db90d982f7576656683fd
kennyyluc <k1luc@ucsd.edu>
2019-06-03 00:12:51 -0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,7 @@
-// This files renders the login.handlebars page when the 
-// login page is requested by app.js
+/*
+ * This files renders the login.handlebars page when the 
+ * login page is requested by app.js
+ */
 
 exports.view = function(req, res){
   res.render('login');
