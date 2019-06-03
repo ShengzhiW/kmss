0462deac6dc998adfe249bde085ecabfe12daa98
Shengzhi Wang <shengzhi@Shengzhis-MacBook-Pro.local>
2019-06-02 19:10:56 -0700

    commit message

---
+++ public/script/facts-functions.js
@@ -0,0 +1,52 @@
+$("#new-fact").click(function () {
+  // alert( "Handler for .click() called." );
+  $.ajax({
+    type: "GET",
+    url: "/facts",
+    data: 'json',
+    success: function (html) {
+      $("#hur-fact").hide().html(html).fadeIn();
+
+      console.log(html)
+      // alert(html);
+    }
+  });
+});
+
+$("#notif").click(function () {
+  Notification.requestPermission().then(function (result) {
+    if (result === 'denied') {
+      console.log('Permission wasn\'t granted. Allow a retry.');
+      return;
+    }
+    if (result === 'default') {
+      console.log('The permission request was dismissed.');
+      return;
+    }
+    // Do something with the granted permission.
+  });
+});
+
+window.cb = function cb(json) {
+  //do what you want with the json
+  $("#home-city").html('Your Location:<br>' + json.address.road + ', ' + json.address.city + '<br>' + json.address.country);
+}
+
+function getgeo() {
+
+  $("#location-loading").toggleClass('hidden');
+  console.log("getting location...")
+  navigator.geolocation.getCurrentPosition(function (location) {
+
+    console.log(location.coords.latitude);
+    console.log(location.coords.longitude);
+    console.log(location.coords.accuracy);
+
+    var s = document.createElement('script');
+    s.src = 'https://nominatim.openstreetmap.org/reverse?json_callback=cb&format=json&lat=' + location.coords.latitude + '&lon=' + location.coords.longitude + '&zoom=27&addressdetails=1';
+    document.getElementsByTagName('head')[0].appendChild(s);
+
+    //Set location cookie
+    document.cookie = location.coords.latitude + "," + location.coords.longitude;
+  });
+}
