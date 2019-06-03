80373da6411ce133c941eeae3c395c9a5d66c161
kennyyluc <k1luc@ucsd.edu>
2019-06-03 00:13:51 -0700

    Update shelters.js

--- routes/shelters.js
+++ routes/shelters.js
@@ -1,5 +1,7 @@
-// This files renders the shelters.handlebars page when the 
-// shelters page is requested by app.js
+/*
+ * This files renders the shelters.handlebars page when the 
+ * shelters page is requested by app.js
+ */
 
 exports.view = function(req, res){
   res.render('shelters');
