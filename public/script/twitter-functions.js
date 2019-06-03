68a33170683c2014e0b7f73e0a2429ca4b880230
kennyyluc <k1luc@ucsd.edu>
2019-06-02 23:28:44 -0700

    Update twitter-functions.js

--- public/script/twitter-functions.js
+++ public/script/twitter-functions.js
@@ -1,3 +1,9 @@
+/* 
+ * This file is used to connect our Twitter API and Twitter node module to our trending page. Every 5 seconds it will grab new tweets
+ * and display it on the page.
+*/
+
+// Parases the link from the text and wraps it all inside an <a> tag
 var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
       function linkify(text) {
           var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
@@ -5,6 +11,8 @@ var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\
               return '<a href="' + url + '">' + url + '</a>';
           });
       }
+
+      // Sets an interval and grabs tweets using an ajax request to grab tweets.
       setInterval( function(){
             console.log("getting tweets");
             $.ajax({
@@ -13,13 +21,17 @@ var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\
                 data: 'json',
                 success: function(html){
                    console.log(html);
+                      
+                   // If nothing
                    if (html.length == 0)
                       return;
-      
+                   
+                   // Set up html elements
                    $('.live-update').html('')
                    $("#live-tweets").addClass('live-tweets-height')
                    $("#live-tweets").html("");
-      
+                  
+                  // Set up each tweet
                   for (let i=0; i<html.length; i++) {
                       setTimeout( function timer(){
                          $("#live-tweets").append('<div class="tweet-ind"><img src="/images/twitter.png" style="width: 20px; margin-right:20px">'+linkify(html[i])+'</div>');
