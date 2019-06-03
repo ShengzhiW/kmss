3d5a119b3d65b718de61e4285496c4d1284d0002
Sierra Gutierrez <scg003@ucsd.edu>
2019-06-02 20:28:24 -0700

    m

--- app.js
+++ app.js
@@ -73,9 +73,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
   extended: false
 }))
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json())
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
